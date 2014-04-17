fcabcf0ad990892421fd828b614e4968026cfc66
sanchen <gengtuo.ygt@gmail.com>
2014-04-18 00:33:00 +0800

    fix

---
+++ assets/php/config.php
@@ -0,0 +1,7 @@
+<?php
+//配置文件
+header('Content-Type: text/html; charset=UTF-8');
+
+$sina_k='3213948790'; //新浪微博应用App Key
+$sina_s='c5e0ff809e6ac72d60de3756c3cdfd51'; //新浪微博应用App Secret
+$callback_url='http://pixystone.github.io/assets/php/callback.php'; //授权回调网址
